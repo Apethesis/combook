27764ddb7484ed6d98f489aa2c2222c0cccd82fa
Apethesis <apethesis@gmail.com>
2022-10-21 21:43:56 +0200

    AUGHHHHHH

--- combook.lua
+++ combook.lua
@@ -1,4 +1,33 @@
 local args = {...}
 local expect = require("cc.expect").expect
+local fuzzy = require("fuzzy")
 expect(1,args[1],"string")
 expect(2,args[2],"string","nil") args[2] = args[2] or "text"
+local links = assert(http.get("https://github.com/Apethesis/combook/raw/main/links.srt"), "Failed to download link list, perhaps you don't have internet?") links = links.readAll()
+local ltbl = textutils.unserialize(links)
+local res
+for k,v in pairs(ltbl[args[2]]) do
+    local fuzrez = fuzzy.fuzzy_match(k,args[1])
+    if fuzrez == 100 then
+        res = assert(http.get(v), "Failed to download book, perhaps you don't have internet?")
+    elseif fuzrez >= 50 then
+        print("Did you mean "..k.." (y/n)")
+        local r = string.lower(read())
+        if r == "y" then
+            res = assert(http.get(v), "Failed to download book, perhaps you don't have internet?")
+        else
+            error("That book does not exist.",0)
+        end
+    else
+        error("That book does not exist.",0)
+    end
+end
+print("Press 1 to exit program.")
+sleep(1)
+print(res.readAll())
+while true do
+    local ex = {os.pullEvent("key")}
+    if ex[2] == keys.one then
+        os.queueEvent("terminate")
+    end
+end
---
+++ fuzzy.lua
@@ -0,0 +1,52 @@
+-- added by compec so 9551 is credited
+--------------------by 9551 DEV------------------------
+---Copyright (c) 2021-2022 9551------------9551Dev#5787
+---using this code in your project is fine!------------
+---as long as you dont claim you made it---------------
+---im cool with it, feel free to include---------------
+---in your projects!   discord: 9551Dev#5787-----------
+---you dont have to but giving credits is nice :)------
+-------------------------------------------------------
+-------------------------------------------------------
+local pretty = require("cc.pretty")
+
+local function fuzzy_match(str, pattern)
+    local part = 100/math.max(#str,#pattern)
+    local str_len = string.len(str)
+    local pattern_len = string.len(pattern)
+    local dp = {}
+    for i = 0, str_len do
+        dp[i] = {}
+        dp[i][0] = i
+    end
+    for j = 0, pattern_len do
+        dp[0][j] = j
+    end
+    for i = 1, str_len do
+        for j = 1, pattern_len do
+            local cost = 0
+            if string.sub(str, i, i) ~= string.sub(pattern, j, j) then
+                cost = 1
+            end
+            dp[i][j] = math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + cost)
+        end
+    end
+    return 100-dp[str_len][pattern_len]*part
+end
+
+local function sort_strings(str_array, pattern)
+    local result,out = {},{}
+    for k, str in pairs(str_array) do
+        table.insert(result,{fuzzy_match(k, pattern),k,str})
+    end
+    table.sort(result, function(a, b) return a[1] > b[1] end)
+    for k,v in ipairs(result) do
+        out[k] = {match=v[1],str=v[2],data=v[3]}
+    end
+    return out
+end
+
+return {
+    fuzzy_match=fuzzy_match,
+    sort_strings=sort_strings,
+}
